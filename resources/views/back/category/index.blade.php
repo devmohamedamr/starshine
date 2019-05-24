774649be26dc88940c7122574d0cf23f3f7f9980
Mohamed Amr <dev.mohamedamr@gmail.com>
2019-05-24 23:55:48 +0200

    initial commit

---
+++ resources/views/back/category/index.blade.php
@@ -0,0 +1,9 @@
+@extends('back.dashboard')
+
+@section('content')
+
+				<h1>category</h1>
+				<div class="listar-description">
+					<p>Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra doloremque laudantium, totam rem aperiam</p>
+				</div>
+@endsection
